2bff843fdef3c65bf6f6b1783d745f1c99653d6b
Lucas Nunes <lucas.nunes@kabum.com.br>
2020-09-12 18:51:13 -0300

    Limpando algumas coisas e adicionando minhas infos

--- src/index.js
+++ src/index.js
@@ -1,100 +1,87 @@
 #!/usr/bin/env node
 
-"use strict";
+'use strict'
 
-const boxen = require("boxen");
-const chalk = require("chalk");
-const inquirer = require("inquirer");
-const clear = require("clear");
-const open = require("open");
+const boxen = require('boxen')
+const chalk = require('chalk')
+const inquirer = require('inquirer')
+const clear = require('clear')
+const open = require('open')
 
-clear();
+clear()
 
-const prompt = inquirer.createPromptModule();
+const prompt = inquirer.createPromptModule()
 
 const questions = [
   {
-    type: "list",
-    name: "action",
-    message: "What you want to do?",
+    type: 'list',
+    name: 'action',
+    message: 'What you want to do?',
     choices: [
       {
-        name: `Send me an ${chalk.green.bold("email")}?`,
+        name: `Send me an ${chalk.green.bold('email')}?`,
         value: () => {
-          open("mailto:ridermansb@gmail.com");
-          console.log("\nDone, see you soon.\n");
+          open('mailto:lukasnunesj@gmail.com')
+          console.log('\nDone, see you soon.\n')
         }
       },
       {
-        name: "Just quit.",
+        name: 'Just quit.',
         value: () => {
-          console.log("Ok, bye.\n");
+          console.log('Ok, bye.\n')
         }
       }
     ]
   }
-];
+]
 
 const data = {
-  name: chalk.bold.green("        Riderman de Sousa Barbosa"),
-  handle: chalk.white("@ridermansb"),
-  work: `${chalk.white("Senior Front-end Developer at")} ${chalk
-    .hex("#2b82b2")
-    .bold("e-Core")}`,
-  blog: chalk.gray("https://medium.com/") + chalk.whiteBright("@ridermansb"),
-  twitter: chalk.gray("https://twitter.com/") + chalk.cyan("ridermansb"),
-  npm: chalk.gray("https://npmjs.com/") + chalk.red("~ridermansb"),
-  github: chalk.gray("https://github.com/") + chalk.green("ridermansb"),
-  linkedin: chalk.gray("https://linkedin.com/in/") + chalk.blue("ridermansb"),
-  web: chalk.cyan("https://about.me/ridermansb"),
-  npx: chalk.red("npx") + " " + chalk.white("ridermansb"),
-
-  labelWork: chalk.white.bold("       Work:"),
-  labelBlog: chalk.white.bold("     Medium:"),
-  labelTwitter: chalk.white.bold("    Twitter:"),
-  labelnpm: chalk.white.bold("        npm:"),
-  labelGitHub: chalk.white.bold("     GitHub:"),
-  labelLinkedIn: chalk.white.bold("   LinkedIn:"),
-  labelWeb: chalk.white.bold("        Web:"),
-  labelCard: chalk.white.bold("       Card:")
-};
+  name: chalk.bold.green('        Lucas Nunes Joaquim'),
+  handle: chalk.white('@lukasnunesj'),
+  work: `${chalk.white('Junior Full-stack Developer at')} ${chalk
+    .hex('#2b82b2')
+    .bold('Kabum!')}`,
+  npm: chalk.gray('https://npmjs.com/') + chalk.red('~lucasnunesj'),
+  github: chalk.gray('https://github.com/') + chalk.green('lukasnunesj'),
+  linkedin: chalk.gray('https://linkedin.com/in/') + chalk.blue('lucasnunesjoaquim'),
+  web: chalk.cyan('https://lucasnunes.dev'),
+  npx: chalk.red('npx') + ' ' + chalk.white('lucasnunesj'),
+  labelWork: chalk.white.bold('       Work:'),
+  labelnpm: chalk.white.bold('        npm:'),
+  labelGitHub: chalk.white.bold('     GitHub:'),
+  labelLinkedIn: chalk.white.bold('   LinkedIn:'),
+  labelWeb: chalk.white.bold('        Web:'),
+  labelCard: chalk.white.bold('       Card:')
+}
 
 const me = boxen(
   [
     `${data.name} / ${data.handle}`,
-    ``,
     `${data.labelWork}  ${data.work}`,
-    `${data.labelBlog}  ${data.blog}`,
-    `${data.labelTwitter}  ${data.twitter}`,
     `${data.labelnpm}  ${data.npm}`,
     `${data.labelGitHub}  ${data.github}`,
     `${data.labelLinkedIn}  ${data.linkedin}`,
     `${data.labelWeb}  ${data.web}`,
-    ``,
     `${data.labelCard}  ${data.npx}`,
-    ``,
     `${chalk.italic(
       "I'm curious, enthusiastic and student most of the time."
     )}`,
-    `${chalk.italic("The rest of the time I write code that others can read.")}`
-  ].join("\n"),
+    `${chalk.italic('The rest of the time I write code that others can read.')}`
+  ].join('\n'),
   {
     margin: 1,
     float: 'center',
     padding: 1,
-    borderStyle: "single",
-    borderColor: "green"
+    borderStyle: 'single',
+    borderColor: 'green'
   }
-);
+)
 
-console.log(me);
+console.log(me)
 const tip = [
-  `Tip: Try ${chalk.cyanBright.bold(
-    "cmd/ctrl + click"
-  )} on the links above`,
-  '',
-].join("\n");
-console.log(tip);
+  `Tip: Try ${chalk.cyanBright.bold('cmd/ctrl + click')} on the links above`,
+  ''
+].join('\n')
+console.log(tip)
 
-
-prompt(questions).then(answer => answer.action());
+prompt(questions).then(answer => answer.action())
